8648b3e3072687aac9a0607b775f8d92aa6d429a
jjemharper <dilyara1806@mail.ru>
2017-06-23 15:06:06 +0300

    Add files via upload

--- exam/exam.py
+++ exam/exam.py
@@ -23,3 +23,21 @@ for path in f:
     file_toWrite.write(path + "\t" + str(len(words_number)) + "\n")
  
 file_toWrite.close()
+
+#Second task
+with open('authors.csv', 'w',  encoding='utf-8') as csvfile:
+    fieldnames = ['Название файла', 'Автор', 'Тематика текста']
+ 
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+ 
+    author_pattern = re.compile(r"(?<=meta content=\").*?(?=\" name=\"author\")")
+    topic_pattern = re.compile(r"(?<=meta content=\").*?(?=\" name=\"topic\")")
+ 
+    for path in f:
+        file = open(os.path.dirname(__file__) + "/news/" + path, 'r', encoding='windows-1251')
+        text = file.read()
+ 
+        author = author_pattern.search(text).group()
+        topic = topic_pattern.search(text).group()
+        writer.writerow({'Название файла' : path, 'Автор':author, 'Тематика текста':topic})
